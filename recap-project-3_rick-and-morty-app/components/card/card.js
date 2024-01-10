34c42af229e55b3bf65e6cebc016e5cf0edf2354
Tina Huynh Le <huynhletina@gmail.com>
2024-01-10 13:22:04 +0100

    implement creatCardFunction, fetch data and rendered cards

--- recap-project-3_rick-and-morty-app/components/card/card.js
+++ recap-project-3_rick-and-morty-app/components/card/card.js
@@ -1,30 +1,31 @@
-export function createCharacterCard() {
-  const cardContainer = document.querySelector('[data-js="card-container"]');
-  cardContainer.classList.add("card-container");
+export default function CreateCharacterCard(characterData) {
   const card = document.createElement("li");
   card.classList.add("card");
 
-  card.innerHTML = `<li class="card">
-          <div class="card__image-container">
-            <img
-              class="card__image"
-              src="${IMG - SRC}"
-              alt="Rick Sanchez"
-            />
-            <div class="card__image-gradient"></div>
-          </div>
-          <div class="card__content">
-            <h2 class="card__title">${CHARACTERNAME}</h2>
-            <dl class="card__info">
-              <dt class="card__info-title">Status</dt>
-              <dd class="card__info-description">${STATUS}</dd>
-              <dt class="card__info-title">Type</dt>
-              <dd class="card__info-description">${TYPE}</dd>
-              <dt class="card__info-title">Occurrences</dt>
-              <dd class="card__info-description">${OCCURENCES}</dd>
-            </dl>
-          </div>
-        </li>`;
+  card.innerHTML = `<div class="card__image-container">
+  <img
+  class="card__image"
+  src="${characterData.image}"
+  alt="Rick Sanchez"
+  />
+  <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+  <h2 class="card__title">${characterData.name}</h2>
+  <dl class="card__info">
+  <dt class="card__info-title">Status</dt>
+  <dd class="card__info-description">${characterData.status}</dd>
+  <dt class="card__info-title">Type</dt>
+  <dd class="card__info-description">${characterData.type}</dd>
+  <dt class="card__info-title">Occurrences</dt>
+  <dd class="card__info-description">${characterData.episode.length}</dd>
+  </dl>
+  </div>`;
+  return card;
+}
 
-  cardContainer.append(card);
+export function renderElement(element) {
+  const cardContainer = document.querySelector('[data-js="card-container"]');
+  cardContainer.classList.add("card-container");
+  cardContainer.appendChild(element);
 }
--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,3 +1,6 @@
+import CreateCharacterCard from "./components/card/card.js";
+import { renderElement } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +15,47 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// Fetching data
+// export function Card(characterData) {
+// const card = document.createElement("article");
+// return card}
+
+// export function renderElement(element) {
+//   root.appendChild(element);
+// }
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    console.log(data);
+    console.log(data.results);
+    console.log(data.results[0]);
+    console.log(data.results[0].image);
+
+    data.results.forEach((card) => {
+      const newCard = CreateCharacterCard(card);
+      renderElement(newCard);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// cardContainer.append(newCard);
+//       function renderElement(element) {
+//         cardContainer.appendChild(element);
+//       }
+
+//     data.results.forEach((characterData) => {
+//       const newCard = CreateCharacterCard(characterData);
+//       renderElement(newCard);
+//     });
+// }
+//  catch (error) {
+//     console.log("error");
+//   }
+// }
+
+fetchCharacters();
