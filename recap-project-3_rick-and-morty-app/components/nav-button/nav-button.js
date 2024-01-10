d64ed981901fce4bad90ef9dbbd110b9a1ca8fa2
Tina Huynh Le <huynhletina@gmail.com>
2024-01-10 14:54:26 +0100

    grab buttons in nav-buttons.js, add event listeners to buttons

---
+++ recap-project-3_rick-and-morty-app/components/nav-button/nav-button.js
@@ -0,0 +1,5 @@
+export const prevButton = document.querySelector('[data-js="button-prev"]');
+prevButton.classList.add("button");
+
+export const nextButton = document.querySelector('[data-js="button-next"]');
+nextButton.classList.add("button");
--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,3 +1,5 @@
+import { prevButton, nextButton } from "./components/nav-button/nav-button.js";
+
 import CreateCharacterCard from "./components/card/card.js";
 import { renderElement } from "./components/card/card.js";
 
@@ -7,8 +9,8 @@ const searchBarContainer = document.querySelector(
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
+// const prevButton = document.querySelector('[data-js="button-prev"]');
+// const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
@@ -22,14 +24,52 @@ function clearCardContainer() {
   console.log("container cleared!");
 }
 
+// THIS WORKS!!!! DO NOT DELETE!!!!!!
 // Fetching data
+// async function fetchCharacters() {
+//   clearCardContainer();
+//   try {
+//     const url = "https://rickandmortyapi.com/api/character";
+//     const page = 1;
+
+//     const response = await fetch(`${url}?page=${page}`);
+//     const data = await response.json();
+
+//     // logging stuff to check
+//     console.log(data);
+//     console.log(data.results);
+//     console.log(data.results[0]);
+//     console.log(data.results[0].image);
+
+//     data.results.forEach((card) => {
+//       const newCard = CreateCharacterCard(card);
+//       renderElement(newCard);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// END OF WORKING CODE
+
+// adding event listeners to buttons
+prevButton.addEventListener("click", () => {
+  console.log("GO BACK!!!");
+});
+nextButton.addEventListener("click", () => {
+  console.log("NEXT!!!");
+});
+
+// Fetching data + pagination
 async function fetchCharacters() {
+  const url = "https://rickandmortyapi.com/api/character";
+  const page = 1;
+
   clearCardContainer();
   try {
-    const url = "https://rickandmortyapi.com/api/character";
-    const page = 1;
     const response = await fetch(`${url}?page=${page}`);
     const data = await response.json();
+
+    // logging stuff to check
     console.log(data);
     console.log(data.results);
     console.log(data.results[0]);
