12dc9c9dc090612e989552fd3baf3c843b3a04ec
Tina Huynh Le <huynhletina@gmail.com>
2024-01-10 15:57:18 +0100

    implement event listeners for refreshing page number

---
+++ recap-project-3_rick-and-morty-app/components/nav-pagination/nav-pagination.js
@@ -0,0 +1,2 @@
+export const pagination = document.querySelector('[data-js="pagination"]');
+pagination.classList.add("navigation__pagination");
--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,5 +1,7 @@
 import { prevButton, nextButton } from "./components/nav-button/nav-button.js";
 
+import { pagination } from "./components/nav-pagination/nav-pagination.js";
+
 import CreateCharacterCard from "./components/card/card.js";
 import { renderElement } from "./components/card/card.js";
 
@@ -11,12 +13,15 @@ const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 // const prevButton = document.querySelector('[data-js="button-prev"]');
 // const nextButton = document.querySelector('[data-js="button-next"]');
-const pagination = document.querySelector('[data-js="pagination"]');
+// const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
+const maxPage = 42;
 const page = 1;
+let currentPage = page;
 const searchQuery = "";
+
+pagination.textContent = currentPage + ` | ${maxPage}`;
 
 // Function to clear the container everytime data is fetched
 function clearCardContainer() {
@@ -51,18 +56,30 @@ function clearCardContainer() {
 // }
 // END OF WORKING CODE
 
-// adding event listeners to buttons
+// adding event listeners to prev button
 prevButton.addEventListener("click", () => {
   console.log("GO BACK!!!");
-});
-nextButton.addEventListener("click", () => {
-  console.log("NEXT!!!");
+  if (currentPage !== page) {
+    currentPage--;
+  } else {
+    currentPage = page;
+  }
+  pagination.textContent = currentPage + ` | ${maxPage}`;
 });
 
+// adding event listener to next button
+nextButton.addEventListener("click", () => {
+  console.log("NEXT!!!");
+  if (currentPage < maxPage) {
+    currentPage++;
+  } else {
+    currentPage = maxPage;
+  }
+  pagination.textContent = currentPage + ` | ${maxPage}`;
+});
 // Fetching data + pagination
 async function fetchCharacters() {
   const url = "https://rickandmortyapi.com/api/character";
-  const page = 1;
 
   clearCardContainer();
   try {
